e8f5c6d5ffb4d5edf6c0ed89956295984d8682fe
Roberta Calegari <roberta.calegari@unibo.it>
2019-10-17 15:26:32 +0000

    Update argumentationEngineInterface.pl

--- src/main/resources/it/unibo/argumentation/arg2p/argumentationEngineInterface.pl
+++ src/main/resources/it/unibo/argumentation/arg2p/argumentationEngineInterface.pl
@@ -8,8 +8,14 @@ go :-
     buildArgumentationGraph([Arguments, Attacks, Supports] ),
     argumentLabelling([Arguments, Attacks, Supports], [IN, OUT, UND]),
     statementLabelling([IN, OUT, UND], _).
-
+    
 go([In, No, Und]) :-
-    time(buildArgumentationGraph([Arguments, Attacks, Supports] )), %Execute Goal just like call/1 and print time used
-    time(argumentLabelling([Arguments, Attacks, Supports], [IN, OUT, UND])),
-    time(statementLabelling([IN, OUT, UND], [In, No, Und])), !.
+    buildArgumentationGraph([Arguments, Attacks, Supports] ),
+    argumentLabelling([Arguments, Attacks, Supports], [IN, OUT, UND]),
+    statementLabelling([IN, OUT, UND], [In, No, Und]), !.
+
+
+%go([In, No, Und]) :-
+%    time(buildArgumentationGraph([Arguments, Attacks, Supports] )), %Execute Goal just like call/1 and print time used
+%    time(argumentLabelling([Arguments, Attacks, Supports], [IN, OUT, UND])),
+%    time(statementLabelling([IN, OUT, UND], [In, No, Und])), !.
