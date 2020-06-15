cb68aed3738e1acad362e5e8e2845154693da212
Roberta Calegari <roberta.calegari@unibo.it>
2020-06-15 15:29:02 +0000

    Update argumentationEngineInterface.pl

--- src/main/resources/it/unibo/argumentation/arg2p/argumentationEngineInterface.pl
+++ src/main/resources/it/unibo/argumentation/arg2p/argumentationEngineInterface.pl
@@ -3,16 +3,37 @@
 % PIKA-lab
 % Year: 2019
 % ---------------------------------------------------------------
-    
-buildLabelSets ([In, No, Und]) :-
+
+buildLabelSets ([In, No, Und], [IN, OUT, UND]) :-
     convertAllRules,
     buildArgumentationGraph([Arguments, Attacks, Supports] ),
     argumentLabelling([Arguments, Attacks, Supports], [IN, OUT, UND]),
     argumentBPLabelling([IN, OUT, UND], [BPIN, BPOUT, BPUND]),
     statementLabelling([BPIN, BPOUT, BPUND], [In, No, Und]), !.
+    
+printLabelSets :-
+    buildLabelSets([In, Out, Und], [IN, OUT, UND]),
+    write('\n==================================================================================> '),write('\n'),
+    write('\n==============================================> STATEMENT LABELLING '),write('\n'),
+    write('\n==================================================================================> '),write('\n'),
+    write('\n==============================================> IN '),write('\n'),
+    writeList(In),write('\n'),
+    write('==============================================> OUT '),write('\n'),
+    writeList(Out),write('\n'),
+    write('==============================================> UND '),write('\n'),
+    writeList(Und),write('\n'),
+    write('\n==================================================================================> '),write('\n'),
+    write('\n==============================================> ARGUMENT LABELLING '),write('\n'),
+    write('\n==================================================================================> '),write('\n'),
+    write('\n==============================================> IN '),write('\n'),
+    writeList(IN),write('\n'),
+    write('==============================================> OUT '),write('\n'),
+    writeList(OUT),write('\n'),
+    write('==============================================> UND '),write('\n'),
+    writeList(UND),write('\n')
 
 answerQuery(Goal, YesResult, NoResult, UndResult) :- convertAllRules,
-													 buildLabelSets([In, Out, Und]),
+													 buildLabelSets([In, Out, Und], [IN, OUT, UND]),
 													 findall(Goal, answerSingleQuery(Goal, In), YesResult),
 													 findall(Goal, answerSingleQuery(Goal, Out), NoResult),
 													 findall(Goal, answerSingleQuery(Goal, Und), UndResult).
