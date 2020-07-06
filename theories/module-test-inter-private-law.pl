9f38e5955ae67cecd4158a02ee544882b0c1f505
Giuseppe Pisano <giuseppe.pisano3@studio.unibo.it>
2020-07-06 16:46:55 +0200

    Fix in example theories

--- theories/module-test-inter-private-law.pl
+++ theories/module-test-inter-private-law.pl
@@ -1,10 +1,8 @@
-%modulesPath('PATH_TO_THE_PROJECT/theories/inter_private_law').
-
-modulesPath('/Users/robertacalegari/Documents/UNIVERSITA/Arg2P-modularity/theories/inter_private_law').
+modulesPath('PATH_TO_MODULES_DIRECTOR/inter_private_law').
 
 % Queries
-% call_module([ListaModuli], decision(Case, Jurisdiction, Decision)).
+% call_module([ListaModuli], decision(Case, Court, Decision)).
 
+% call_module(['top-italy'], decision('rossiVjones', X, hasToPay(Y, Z))).
 % call_module(['top-italy'], decision('rossiVjones', tribunalOfBologna, hasToPay(rossiSpa, 20000))).
 % call_module(['top-italy'], decision('rossiVjones', tribunalOfBologna, hasToPay(rossiSpa, 10000))).
-% call_module(['top-italy'], decision('rossiVjones', tribunalOfBologna, hasToPay(rossiSpa, X))).
