cd324d623dd86c88ae85cf7c437a755fb444325c
Giuseppe Pisano <giuseppe.pisano3@studio.unibo.it>
2020-09-24 16:43:55 +0200

    Sort bug fixed, negation in answerQuery/4

--- arg2p/src/main/resources/it/unibo/argumentation/arg2p/argumentationEngineInterface.pl
+++ arg2p/src/main/resources/it/unibo/argumentation/arg2p/argumentationEngineInterface.pl
@@ -4,6 +4,8 @@
 % Year: 2019
 % ---------------------------------------------------------------
 
+argTuProlog.
+
 buildLabelSets :-
     buildLabelSets([In, Out, Und], [IN, OUT, UND]),
     write('\n=================================================>'),nl,
@@ -29,7 +31,7 @@ buildLabelSets :-
 
 buildLabelSets([In, No, Und], [BPIN, BPOUT, BPUND]) :-
     convertAllRules,
-    buildArgumentationGraph([Arguments, Attacks, Supports] ),
+    buildArgumentationGraph([Arguments, Attacks, Supports]),
     argumentLabelling([Arguments, Attacks, Supports], [IN, OUT, UND]),
     argumentBPLabelling([IN, OUT, UND], [BPIN, BPOUT, BPUND]),
     statementLabelling([BPIN, BPOUT, BPUND], [In, No, Und]), !,
@@ -43,7 +45,8 @@ answerQuery(Goal, YesResult, NoResult, UndResult) :-
     findall(Goal, answerSingleQuery(Goal, Und), UndResult).
 
 answerSingleQuery(Goal, Set) :-
-    member([Goal], Set).
+    check_modifiers_in_list([Goal], [X]),
+    member(X, Set).
 
 %go([In, No, Und]) :-
 %    time(buildArgumentationGraph([Arguments, Attacks, Supports] )), %Execute Goal just like call/1 and print time used
