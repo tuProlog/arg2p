53fee32f1b6b6ab4a652878034306f90e640655c
Giuseppe Pisano <g.pisano@unibo.it>
2020-10-22 17:47:00 +0200

    base defeasible preferences working

--- arg2p/src/main/resources/it/unibo/argumentation/arg2p/core/labellings/argument/groundedDefeasiblePreferences.pl
+++ arg2p/src/main/resources/it/unibo/argumentation/arg2p/core/labellings/argument/groundedDefeasiblePreferences.pl
@@ -1,42 +1,60 @@
-% ----------------------------------------------------------------
-% argumentLabelling.pl
-% PIKA-lab
-% Year: 2019
-% ---------------------------------------------------------------
+argumentGroundedDefeasiblePreferencesLabelling([Arguments, Attacks, _], [IN, OUT, UND]) :-
+    labelArgumentsGDP(Arguments, Attacks, [], [], Arguments, IN, OUT, UND), !.
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-argumentGroundedDefeasiblePreferencesLabelling([Arguments, _, _], [IN, OUT, UND]) :-
-    labelArgumentsGDP(Arguments, [], [], IN, OUT, UND), !.
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-labelArgumentsGDP(UND, IN, OUT, ResultIN, ResultOUT, ResultUND) :-
-
-   findall(A, ( member(A, UND), allAttacksOUT(A, OUT) ), NewListIN),
-   append(IN, NewListIN, NewIN),
-   findall(A, ( member(A, UND), oneAttackIN(A, NewIN) ), NewListOUT),
-   append(OUT, NewListOUT, NewOUT),
-
-   append(NewListIN, NewListOUT, NewLabelledArguments),
-   \+ isEmptyList(NewLabelledArguments),
-
-   subtract(UND, NewLabelledArguments, UndPlus1),
-   labelArgumentsGDP(UndPlus1, NewIN, NewOUT, ResultIN, ResultOUT, ResultUND).
-
-labelArgumentsGDP(UND, IN, OUT, IN, OUT, UND).
+labelArgumentsGDP(Arguments, Attacks, IN, OUT, UND, ResultIN, ResultOUT, ResultUND) :-
+    member(A, UND), 
+    allAttacksOUT(Attacks, A, OUT),
+    append(IN, [A], NewIN),
+    subtract(UND, [A], NewUND),
+    expandPreferenceSet(A, Arguments, Attacks, NewAttacks),
+    labelArgumentsGDP(Arguments, NewAttacks, NewIN, OUT, NewUND, ResultIN, ResultOUT, ResultUND).
+labelArgumentsGDP(Arguments, Attacks, IN, OUT, UND, ResultIN, ResultOUT, ResultUND) :-
+    member(A, UND), 
+    oneAttackIN(Attacks, A, IN),
+    append(OUT, [A], NewOUT),
+    subtract(UND, [A], NewUND),
+    labelArgumentsGDP(Arguments, Attacks, IN, NewOUT, NewUND, ResultIN, ResultOUT, ResultUND).
+labelArgumentsGDP(_, _, IN, OUT, UND, IN, OUT, UND).
 
 /*
     If an attack exists, it should come from an OUT argument
 */
-allAttacksOUT(A, OUT) :-
-    \+ ( attack(B, A), \+ ( member(B, OUT))).
+allAttacksOUT(Attacks, A, OUT) :-
+    \+ ( member((B, A), Attacks), \+ ( member(B, OUT))).
 
 /*
     Find an attack, if exists, from an IN argument, then ends
 */
-oneAttackIN(A, IN) :-
-    attack(B, A),
+oneAttackIN(Attacks, A, IN) :-
+    member((B, A), Attacks),
     member(B, IN), !.
+
+expandPreferenceSet([_, _, [sup(RuleOne, RuleTwo)]], Arguments, Attacks, NewAttacks) :-
+    write(RuleOne), nl,
+    write(RuleTwo), nl,
+    findall(A, computeInvalidAttacks(RuleOne, RuleTwo, Arguments, Attacks, A), IA),
+    appendLists(IA, InvalidAttacks),
+    subtract(Attacks, InvalidAttacks, NewAttacks).
+
+expandPreferenceSet(_, _, Attacks, Attacks).
+
+computeInvalidAttacks(RuleOne, RuleTwo, Arguments, Attacks, InvalidAttacks) :-
+    member([IdOne, RuleOne, COne], Arguments),
+    member([IdTwo, RuleTwo, CTwo], Arguments),
+    findDerivedArguments([IdTwo, RuleTwo, CTwo], Arguments, Derived),
+    findall((A, [IdOne, RuleOne, COne]), member(A, Derived), TempInvalidAttacks),
+    append([([IdTwo, RuleTwo, CTwo], [IdOne, RuleOne, COne])], TempInvalidAttacks, InvalidAttacks).
+
+findDerivedArguments([IdTwo, _, _], Arguments, Derived) :-
+    findall([Id, T, C], (member([Id, T, C], Arguments), subset(IdTwo, Id), IdTwo \== Id), Derived).
+
+subset([], _).
+subset([H|T], List) :- member(H, List), subset(T,List).
+
+% Defeasible preference addition
+conflict([sup(X, Y)],  [sup(Y, X)]).
 
 % Se vado in IN controllo la possibile presenza di una indicazione di preferenza
 %    sup(r1, r2)
